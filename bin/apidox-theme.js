dff48f217fc612c266fc00a7c9bd3cc4fbb7be9b
Fangli Xie <xfloops@gmail.com>
2018-12-03 16:28:18 +0800

    feat: add theme cli command

---
+++ bin/apidox-theme.js
@@ -0,0 +1,35 @@
+#!/usr/bin/env node
+
+const path = require('path')
+const fs = require('fs')
+const less = require('less')
+const chalk = require('chalk')
+
+const src = f => path.join(__dirname, '../theme', f)
+const dist = f => path.join(__dirname, '../templates/style', f)
+const readFile = f => fs.readFileSync(src(f), {encoding: 'utf8'})
+const writeFile = (f, c) => fs.writeFileSync(dist(f), c, {encoding: 'utf8'})
+
+const defaultVariables = readFile('variables-default.less')
+const defaultLayout = readFile('layout-default.less')
+
+const themeLess= theme => defaultVariables + (theme === 'default' ? '' : readFile('variables-' + theme + '.less')) + defaultLayout
+const themes = ['default', 'cyborg', 'flatly', 'slate', 'streak']
+
+themes.forEach((theme) => {
+  let lessText = themeLess(theme)
+
+  less.render(lessText, {compress: true}, (err, result) => {
+    if (err) {
+      console.log(err)
+    }
+
+    let input = result.css
+    let output = theme + '.css'
+
+    writeFile(output, input)
+
+    console.log(chalk.green(`[ ${output} ] is created.`))
+  })
+})
+
--- src/utils/doc-router.js
+++ src/utils/doc-router.js
@@ -9,6 +9,8 @@ const markdownit = require('markdown-it')({
 
 module.exports = (bridge) => {
   const router = express.Router()
+  const apiJson = bridge.apiJson
+  const dirs = bridge.dirs
   const locals = {
     self: true, // if ture, all locals variables are accessed by self.variable.
     debug: false,
@@ -16,8 +18,16 @@ module.exports = (bridge) => {
     highlight
   }
 
-  router.use('/example', (req, res, next) => {
-    res.render('index', locals)
+  router.use('/:page', (req, res, next) => {
+    let page = req.params.page
+    // [ ]fixme: unmatched route make api return undefined
+    let api = apiJson['/' + page] || apiJson['/'] || apiJson[dirs[0]]
+
+    res.render('page', { ...locals, api })
+  })
+
+  router.use('/', (req, res, next) => {
+    res.redirect('/index')
   })
 
   return router
